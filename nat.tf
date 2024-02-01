89ce863073f6c55fe902c65ce349a7f33b1e5e0f
aitzol <anaberan@gmail.com>
2024-02-01 10:53:35 +0000

    Azpisare privatua

---
+++ nat.tf
@@ -0,0 +1,3 @@
+resource "aws_eip" "external-ip" {
+  domain = "vpc"
+}
--- vpc.tf
+++ vpc.tf
@@ -36,6 +36,36 @@ resource "aws_route_table_association" "asoc_public-1" {
     route_table_id = aws_route_table.tabla-de-enrutamiento.id
 }
 
+resource "aws_subnet" "subred-pr-1" {
+    vpc_id = aws_vpc.primera-vpc.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = "false"
 
+    tags = {
+        name = "Subred Privada 1"
+    }
+}
 
+resource "aws_nat_gateway" "mi-nat" {
+    subnet_id = aws_subnet.subred-1.id
+    allocation_id = aws_eip.external-ip.id
+    depends_on = [ aws_internet_gateway.gateway-1]
+}
+
+resource "aws_route_table" "route-table-privada" {
+    vpc_id = aws_vpc.primera-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.mi-nat.id
+    }
+}
+
+resource "aws_route_table_association" "asoc_priv-1" {
+
+    subnet_id = aws_subnet.subred-pr-1.id
+    route_table_id = aws_route_table.route-table-privada.id
+ 
+}
 
